efa41250d0843fb0bbc30728a922c4cd88a2526c
Gabriel M. Dutra <nulldutra@gmail.com>
2024-08-24 23:40:25 -0300

    api-gateway: Add prototype

---
+++ api-gateway/main.tf
@@ -0,0 +1,32 @@
+resource "aws_apigatewayv2_api" "main" {
+  name                       = var.name
+  protocol_type              = "HTTP"
+}
+
+resource "aws_apigatewayv2_domain_name" "main" {
+  domain_name = var.domain_name
+}
+
+resource "aws_apigatewayv2_stage" "main" {
+  api_id      = aws_apigatewayv2_api.main.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_api_mapping" "main" {
+  api_id      = aws_apigatewayv2_api.main.id
+  domain_name = aws_apigatewayv2_domain_name.main.id
+  stage       = aws_apigatewayv2_stage.main.id
+}
+
+resource "aws_route53_record" "main" {
+  name    = aws_apigatewayv2_domain_name.main.domain_name
+  type    = "A"
+  zone_id = aws_route53_zone.main.zone_id
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.main.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.main.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
---
+++ api-gateway/variables.tf
@@ -0,0 +1,9 @@
+variable name {
+  type        = string
+  description = "The name of the API Gateway"
+}
+
+variable domain_name {
+    type = string
+    description = "The domain name to API Gateway custom domain"
+}
